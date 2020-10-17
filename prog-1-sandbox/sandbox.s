6dd04d14c19bdec0afce8aa5b955a9b262b324b1
Lewis Kelly <kellyl64@tcd.ie>
2020-10-17 15:35:34 +0100

    fixes

--- prog-1-sandbox/sandbox.s
+++ prog-1-sandbox/sandbox.s
@@ -8,15 +8,17 @@ Main:
   @ Write your program here
 
   @2x^2+6xy+3y^2
-  LDR R3, #2
+  MOV R3, #2
   MUL R5, R1, R1
   MUL R5, R5, R3
-  LDR R4, #6
-  MUL R6, R4, R1, R2
-  LDR R8, #3
+  MOV R4, #6
+  MUL R6, R4, R1
+  MUL R6, R6, R2
+  MOV R8, #3
   MUL R9, R2, R2
   MUL R9, R9, R8
-  ADD R0, R9, R6, R5  
+  ADD R0, R9, R6
+  ADD R0, R0, R5
 
   @ End of program ... check your result
 
