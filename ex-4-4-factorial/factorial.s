39aa6d0612aa3310847dcd78fbc72839328d3bcc
Lewis Kelly <kellyl64@tcd.ie>
2020-10-20 01:04:04 +0100

    Factorial

--- ex-4-4-factorial/factorial.s
+++ ex-4-4-factorial/factorial.s
@@ -7,9 +7,15 @@ Main:
 
   @ Calculate n!, where n is stored in R1
   @ Store the result in R0
-
-  @ *** your solution goes here ***
-
+  MOV R0, #1
+  MOV R2, R1
+WhileMul:
+  CMP R2, #1
+  BLS EndWhMul
+  MUL R0, R0, R2
+  SUB R2, R2, #1
+  B WhileMul
+EndWhMul:
   @ End of program ... check your result
 
 End_Main:
