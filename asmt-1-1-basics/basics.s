e0eac9f77de66e6b7f23e2cbcad50aa153a95811
Lewis Kelly <kellyl64@tcd.ie>
2020-10-21 09:47:24 +0100

    assignment1.1final

--- asmt-1-1-basics/basics.s
+++ asmt-1-1-basics/basics.s
@@ -7,16 +7,20 @@ Main:
 
   @ Write an ARM Assembly Language program to evaluate
   @   x^3 - 4x^2 + 3x + 8
-  LDR R0, #0
-  MUL R5, R1, R1
-  MUL R5, R5, R1
+  MOV R0, #0
+  MOV R2, #0
+  MOV R3, #0
+  MOV R4, #0
+  MOV R5, #0
+  MUL R5, R1, R1 
+  MUL R5, R5, R1 @x^3
   MOV R2, #4
-  ADD R3, R1
-  MUL R3, R3, R3
-  MUL R2, R2, R3
-  SUB R2, R5, R2
+  ADD R3, R1 @copy x to r3
+  MUL R3, R3, R3 @x^2
+  MUL R2, R2, R3 @4(x^2)
+  SUB R2, R5, R2 @(x^3)-(4x^2)
   MOV R4, #3
-  MUL R4, R4, R1
+  MUL R4, R4, R1 @
   ADD R2, R4, R2
   ADD R0, R2, #8
   @ End of program ... check your result
