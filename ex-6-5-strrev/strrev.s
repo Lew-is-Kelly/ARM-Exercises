55b83d06e90c560a35557867d4eec20dde528c8a
Lewis Kelly <kellyl64@tcd.ie>
2020-11-20 09:52:48 +0000

    rev

--- ex-6-5-strrev/strrev.s
+++ ex-6-5-strrev/strrev.s
@@ -16,13 +16,42 @@ Main:
   @   Add the following watch expression: (unsigned char [64]) stringB
   @
   @   OR
-  @
   @   Open a Memory View specifying the address 0x20000000 and length at least 11
   @   You can open a Memory View with ctrl-shift-p type view memory (cmd-shift-p on a Mac)
   @
 
+  MOV     R3, #0
+  MOV     R4, #0
+  MOV     R2, R1
+
+Loop:          
+  LDRB    R5, [R2]          
+  CMP     R5, #0            
+  BEQ     For                 
+  ADD     R4, R4, #1
+  ADD     R2, R2, #1        
+  B       Loop      
+
+For:
+  ADD     R0, R0, R4
+  MOV     R6, #0
+  STRB    R6, [R0]
+  SUB     R0, R0, #1
+
+ForLoop:
+  LDRB    R3, [R1]
+  CMP     R3, #0
+  BEQ     End
+  STRB    R3, [R0]
+  ADD     R1, R1, #1
+  SUB     R0, R0, #1
+  B       ForLoop
+
+End:
+
   @ End of program ... check your result
 
 End_Main:
+
   BX    lr
 
