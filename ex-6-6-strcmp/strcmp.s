3b9e24d63d7f388ac2f275eb59d805f09d7adb00
Lewis Kelly <kellyl64@tcd.ie>
2020-11-28 19:09:25 +0000

    66

--- ex-6-6-strcmp/strcmp.s
+++ ex-6-6-strcmp/strcmp.s
@@ -9,6 +9,34 @@ Main:
   @
   @ write your program here
   @
+  
+While:
+  LDRB    R3, [R1]
+  LDRB    R4, [R2]
+  CMP     R3, #0
+  BEQ     EndWh
+  CMP     R3, R4
+  BNE     EndWh
+  ADD     R1, R1, #1
+  ADD     R2, R2, #1
+  B       While
+EndWh:
+
+  CMP     R3, R4
+  BNE     EndIf
+  MOV     R0, #0
+  B       End
+EndIf:
+
+  CMP     R3, R4
+  BLS     EndElse
+  MOV     R0, #1
+  B       End
+EndElse:
+
+  MOV     R0, #-1
+
+End:
 
   @ End of program ... check your result
 
