213748d4d90504220b023620b65c17c82ee47bcb
Lewis Kelly <kellyl64@tcd.ie>
2020-12-09 21:11:50 +0000

    swap

--- ex-7-2-swap/swap.s
+++ ex-7-2-swap/swap.s
@@ -13,9 +13,16 @@ Main:
   @   should change R4 to 0x89CDABEF
   @
 
-  @ *** your solution goes here ***
-
-  @ End of program ... check your result
+  MOV   R3, R4, LSR #8    @ 0x0089ABCD
+  MOV   R2, R4, LSL #8    @ 0xABCDEF00
+  LDR   R1, =0x0000FF00
+  LDR   R0, =0x00FF0000
+  AND   R3, R1, R3        @ 0x0000AB00  
+  AND   R2, R0, R2        @ 0x00CD0000
+  EOR   R3, R2, R3        @ 0x00CDAB00
+  LDR   R1, =0xFF0000FF
+  AND   R4, R4, R1        @ 0x890000EF
+  EOR   R4, R4, R3        @ 0x89CDABEF
 
 End_Main:
   BX    lr
