c595f65a7eee1c0eb59c58abebaba3050d50102d
Lewis Kelly <kellyl64@tcd.ie>
2020-12-10 21:09:06 +0000

    61

--- asmt-6-1-count1s/count1s.s
+++ asmt-6-1-count1s/count1s.s
@@ -5,13 +5,30 @@
 
 Main:
 
-  
-  @
-  @ write your program here
-  @
+  MOV     R0, #0            @ amountOfOnes = 0;
+  MOV     R2, #0            @
 
+  MOV     R4, #0            @ i = 0;
+Count:                      @ while(i < 32)
+  CMP     R4, #32           @ {
+  BHS     EndCount          @ 
 
-  @ End of program ... check your result
+  MOVS    R1, R1, LSL #1    @     checkBit = num.MostSignificant;
+  BCC     EndIf             @     if(checkBit==1) {
+  ADD     R2, R2, #1        @         tempOnes++;
+  B       ElseSkip          @
+EndIf:                      @     } else {
+  MOV     R2, #0            @     tempOnes = 0;
+ElseSkip:                   @     }
+
+  CMP     R2, R0            @     if(tempOnes>amountOfOnes)
+  BLO     EndIf2            @     {
+  MOV     R0, R2            @     amountOfOnes = tempOnes;
+EndIf2:                     @     }
+
+  ADD     R4, R4, #1        @     i++;
+  B       Count             @
+EndCount:                   @ }
 
 End_Main:
   BX    lr
