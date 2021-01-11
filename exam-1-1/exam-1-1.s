7949e70ef7ab30c942d0411f6f11044ac6a8492f
Lewis Kelly <kellyl64@tcd.ie>
2021-01-11 15:17:46 +0000

    Exam1

--- exam-1-1/exam-1-1.s
+++ exam-1-1/exam-1-1.s
@@ -11,6 +11,127 @@ Main:
   @ Write your program here
   @
 
+  @ R0 = adrC
+  @ R1 = adrA
+  @ R2 = adrB
+  @ R3 = elemA
+  @ R4 = elemB
+  @ R5 = sizeA
+  @ R6 = sizeB
+  @ R7 = bool
+  @ R8 = countC
+  @ R9 = countT
+  @ R10 = adrTC 
+  @ R11 = adrTA
+  @ R12 = adrTB
+
+  LDR     R5, [R1]          @ sizeA = word[adrA];
+  ADD     R1, R1, #4        @ adrA = adrA + 4;
+  LDR     R6, [R2]          @ sizeB = word[adrB];
+  ADD     R2, R2, #4        @ adrB = adrB + 4;
+  MOV     R11, R1           @ adrTA = adrA;
+  MOV     R12, R2           @ adrTB = adrB;
+
+  MOV     R8, #0            @ countC = 0;
+  MOV     R9, #0            @ countT = 0;
+
+  CMP     R5, #0            @ if(sizeA == 0)
+  BNE     HasDigits         @ {
+  CMP     R6, #0            @     if(sizeB == 0)
+  BNE     HasDigits         @     {
+  B       EndLoopB          @         continue End;
+HasDigits:                  @     } 
+                            @ }
+
+  MOV     R10, R0           @ adrTC = adrC;
+  ADD     R0, R0, #4        @ adrC = adrC + 4;
+
+  STR     R9, [R10]         @ word[adrTC] = countT;
+  
+LoopA:                      @ while(sizeA != countT)
+  LDR     R3, [R1]          @ {   elemA = word[adrA];
+  CMP     R5, R9            @
+  BEQ     EndLoopA          @
+  MOV     R7, #0            @     boolean bool = false;
+  MOV     R2, R12           @     adrB = adrTB;
+  MOV     R9, #0            @     countT = 0;
+
+LoopAB:                     @     while(sizeB != countT)
+  LDR     R4, [R2]          @     {   elemB = word[adrB];
+  CMP     R6, R9            @
+  BEQ     EndLoopAB         @         
+
+  CMP     R3, R4            @         if(elemA == elemB)
+  BNE     EndIfSame         @         {   
+  MOV     R7, #1            @             bool = true;
+EndIfSame:                  @         }
+
+  ADD     R2, R2, #4        @         adrB = adrB + 4;
+  ADD     R9, R9, #1        @         countT++;
+  B       LoopAB            @
+EndLoopAB:                  @     }
+
+  CMP     R7, #1            @     if(bool)
+  BEQ     EndIfBool         @     {
+  STR     R3, [R0]          @         word[adrC] = elemA;
+  ADD     R8, R8, #1        @         countC++;
+  ADD     R0, R0, #4        @         adrC = adrC + 4;
+EndIfBool:                  @     }
+
+  ADD     R1, R1, #4        @     adrA = adrA + 4;
+  LDR     R9, [R10]         @     countT = word[adrTC] 
+  ADD     R9, R9, #1        @     countT++;
+  STR     R9, [R10]         @     word[adrTC] = countT;
+  B       LoopA             @ }
+EndLoopA:                   @
+
+  MOV     R2, R12           @ adrB = adrTB;
+  MOV     R1, R11           @ adrA = adrTA;
+  MOV     R9, #0            @ countT = 0;
+  STR     R9, [R10]         @ word[adrTC] = countT;
+
+LoopB:                      @ while(sizeB != countT)
+  LDR     R4, [R2]          @ {   elemB = word[adrB];
+  CMP     R6, R9            @
+  BEQ     EndLoopB          @
+  MOV     R7, #0            @     bool = false;
+  MOV     R1, R11           @     adrA = adrTA;
+  MOV     R9, #0            @     countT = 0;
+
+LoopBA:                     @     while(sizeA != countT)
+  LDR     R3, [R1]          @     {   elemA = word[adrA];
+  CMP     R5, R9            @
+  BEQ     EndLoopBA         @         
+
+  CMP     R4, R3            @         if(elemB == elemA)
+  BNE     EndIfSame2        @         {   
+  MOV     R7, #1            @             bool = true;
+EndIfSame2:                 @         }
+
+  ADD     R1, R1, #4        @         adrA = adrA + 4;
+  ADD     R9, R9, #1        @         countT++;
+  B       LoopBA            @
+EndLoopBA:                  @     }
+
+  CMP     R7, #1            @     if(bool)
+  BEQ     EndIfBool2        @     {
+  STR     R4, [R0]          @         word[adrC] = elemB;
+  ADD     R8, R8, #1        @         countC++;
+  ADD     R0, R0, #4        @         adrC = adrC + 4;
+EndIfBool2:                 @     }
+
+  ADD     R2, R2, #4        @     adrB = adrB + 4;
+  LDR     R9, [R10]         @     countT = word[adrTC] 
+  ADD     R9, R9, #1        @     countT++;
+  STR     R9, [R10]         @     word[adrTC] = countT;
+  B       LoopB             @ }
+EndLoopB:                   @ End:
+
+  MOV     R0, R10           @ adrC = adrTC;
+  STR     R8, [R0]          @ word[adrC] = countC;
+  
+
+
   @
   @ REMEMBER: Sets are stored in memory in the format ...
   @
