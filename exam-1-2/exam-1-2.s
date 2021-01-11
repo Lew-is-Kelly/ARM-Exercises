471f967c88bbb430f12fa41cb996af44808f24dd
Lewis Kelly <kellyl64@tcd.ie>
2021-01-11 17:09:26 +0000

    exam2

--- exam-1-2/exam-1-2.s
+++ exam-1-2/exam-1-2.s
@@ -11,6 +11,58 @@ Main:
   @ write your program here
   @
 
+  @ R0 = adrN
+  @ R1 = adrO
+  @ R2 = char
+  @ R3 = space
+
+  MOV     R3, #0            @ Boolean space = false;
+  
+While:                      @ while(char != 0)
+  LDRB    R2, [R1]          @ {   char = byte[adrO];
+  CMP     R2, #0            @
+  BEQ     EndWhile          @
+
+  CMP     R2, #0x20         @     if(char == 0x20)
+  BNE     NotSpace          @     {
+  MOV     R3, #1            @         space = true;
+  B       EndIfStr          @         continue GoNext;
+NotSpace:                   @     }
+
+  CMP     R2, #0x41         @     if(char >= 0x41 && char <= 0x5a)
+  BLO     EndIfCap          @     {   
+  CMP     R2, #0x5a         @ 
+  BHI     EndIfCap          @
+
+  CMP     R3, #1            @         if(space)
+  BNE     Space             @         {
+  STRB    R2, [R0]          @             byte[adrN] = char;
+  ADD     R0, R0, #1        @             adrN++;
+  MOV     R3, #0            @             space = false;
+  B       EndIfStr          @             continue GoNext;
+Space:                      @         }
+
+  ADD     R2, R2, #0x20     @         char = char + 0x30
+EndIfCap:                   @     }
+
+  CMP     R2, #0x61         @     if(char >= 0x61 && char <= 0x7a)
+  BLO     EndIfStr          @     {
+  CMP     R2, #0x7a         @
+  BHI     EndIfStr          @
+
+  CMP     R3, #1            @         if(space)
+  BNE     Space2            @         {   
+  SUB     R2, R2, #0x20     @             char = char - 0x20;
+  MOV     R3, #0            @             space = false;
+Space2:                     @         }
+
+  STRB    R2, [R0]          @         byte[adrN] = char;
+  ADD     R0, R0, #1        @         adrN++;
+EndIfStr:                   @      }  
+
+  ADD     R1, R1, #1        @      GoNext: adrO++;
+  B       While             @
+EndWhile:                   @ }
 
   @
   @ Debugging tips:
