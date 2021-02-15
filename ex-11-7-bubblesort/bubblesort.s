b46cf082cf490e84720930b17236c51fba6915a6
Lewis Kelly <kellyl64@tcd.ie>
2021-02-15 12:14:55 +0000

    bubblesort

--- ex-11-7-bubblesort/bubblesort.s
+++ ex-11-7-bubblesort/bubblesort.s
@@ -10,9 +10,19 @@
 
 Main:
 
-@
-@ write your bubblesort program here
-@
+DoLoop:             @ do {
+  MOV     R12, #0   @   swapped = false;
+For:                @   for (i = 1; i < N; i++) {
+  MOV     R10, #0   @
+  LDR     R11, [R1] @
+  CMP               @       if (array[i−1] > array[i]) {
+                    @           tmpswap = array[i−1];
+                    @           array[i−1] = array[i];
+                    @           array[i] = tmpswap;
+  MOV     R12, #1   @           swapped = true ;
+                    @       }
+EndFor:             @   }
+EndLoop:            @ } while ( swapped );
 
 End_Main:
   BX      LR
